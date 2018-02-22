8102b65ef9f8fb606929563c1ebceac470486e0a
Raji <rajalakshmi612@gmail.com>
2018-02-22 23:12:34 +0530

    Admin Panel implementation

---
+++ admin/footer.php
@@ -0,0 +1,3 @@
+<div class="footer pt-20">
+	&copy; <?php echo date('Y'); ?> Design and Development <a href="http://www.myskoo.com" target="_blank">MySkoo</a>&nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp;
+</div>
